181dd1ad3339aae30b1f926b59a8864a43463d54
vlapwd <iyu@vlapwd.com>
2020-06-12 23:37:54 +0900

    update

---
+++ src/components/layout.js
@@ -0,0 +1,41 @@
+import React from "react"
+import { css } from "@emotion/core"
+import { useStaticQuery, Link, graphql } from "gatsby"
+
+import { rhythm } from "../utils/typography"
+export default function Layout({ children }) {
+  const data = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+          }
+        }
+      }
+    `
+  )
+  return (
+    <div
+      css={css`
+        margin: 0 auto;
+        max-width: 90%;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};
+      `}
+    >
+      <Link to={`/`}>
+        <h1
+          css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;
+          `}
+        >
+          {data.site.siteMetadata.title}
+        </h1>
+      </Link>
+      {children}
+    </div>
+  )
+}
---
+++ src/templates/blog-post.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { graphql } from "gatsby"
+import Layout from "../components/layout"
+
+export default function BlogPost({ data }) {
+  const post = data.markdownRemark
+  return (
+    <Layout>
+      <div>
+        <h4>{post.frontmatter.date}</h4>
+        <h2>## {post.frontmatter.title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: post.html }} />
+      </div>
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date
+      }
+    }
+  }
+`
